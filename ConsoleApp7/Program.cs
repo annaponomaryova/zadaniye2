83830cf88f6bfe3e1aba79fcc0e62b9db52e1ec4
КУПК ИТ <kupc_visual@outlook.com>
2020-09-28 13:09:24 +0500

    Commin solution and config files

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApp7
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello world");
+            Console.ReadLine();
         }
     }
 }
